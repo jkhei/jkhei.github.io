2917d14a08d4d2632594dafab22cb611d17f4ef7
Heim <jkheimlaude@gmail.com>
2021-08-13 14:58:33 +0800

    Changes in the test website layout

---
+++ js/myScript.js
@@ -0,0 +1,52 @@
+var banner = document.getElementById('banner');
+var paragraph = document.getElementById('paragraphChange');
+
+function changeText1(){
+	document.getElementById("headerChange").innerHTML = "Lorem Ipsum Dolor";
+	document.getElementById("paragraphChange").innerHTML = "Nunc molestie pretium massa at aliquam. Aenean mattis augue id elementum rhoncus. In placerat elit risus, id mollis metus scelerisque sit amet. Donec luctus, ante id consequat feugiat, ipsum diam malesuada justo, et tempus risus tellus vitae metus. Quisque consectetur diam nec ligula volutpat tempor. Donec tellus nulla, interdum quis magna non, lobortis mollis turpis.";
+    
+    banner.style.background = '#333333';
+    paragraph.style.color = '#EAEAEA'
+}
+
+
+function changeText2(){
+	document.getElementById("headerChange").innerHTML = "Lorem Alpha";
+	document.getElementById("paragraphChange").innerHTML = "Sed rutrum ullamcorper congue. Sed suscipit, lacus lobortis efficitur porta, justo mi tempor risus, non sagittis odio tellus ac nisl. Sed fringilla est id erat pulvinar ullamcorper. Aliquam consectetur pretium risus id accumsan. Vivamus neque ex, ultrices eu iaculis id, sagittis vitae odio. Morbi sodales mi mattis, porta nunc sed, malesuada nisl.";
+    
+    banner.style.background = '#46A9F0';
+    paragraph.style.color = '#222222'
+}
+
+function changeText3(){
+	document.getElementById("headerChange").innerHTML = "Lorem Beta";
+	document.getElementById("paragraphChange").innerHTML = "Aliquam quis commodo nunc. Aliquam mi magna, laoreet et aliquam sit amet, tempus non augue. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut et arcu gravida, hendrerit leo quis, accumsan odio. Praesent posuere tortor vel tristique volutpat. Fusce sed lacus turpis. Nunc nec metus vitae risus tempor fermentum. Vivamus nec purus nec velit commodo tempus commodo eget mi. Maecenas quis iaculis diam.";
+    
+    banner.style.background = '#2FF791';
+    paragraph.style.color = '#222222'
+}
+
+function changeText4(){
+	document.getElementById("headerChange").innerHTML = "Lorem Gamma";
+	document.getElementById("paragraphChange").innerHTML = "Maecenas fringilla vitae ante nec faucibus. Suspendisse mollis scelerisque orci, ac mattis nulla imperdiet vitae. Donec id sodales ligula, sed pulvinar ante. Nam accumsan ultrices ante nec posuere. Integer porta dui ut sapien malesuada, quis ornare odio malesuada. Nullam ut congue metus, vitae tincidunt velit. Cras eu egestas sem. Ut tincidunt tristique mauris ut pretium. Curabitur arcu ipsum, congue vitae libero et, malesuada tempus magna.";
+    
+    banner.style.background = '#99E036';
+    paragraph.style.color = '#222222'
+}
+
+function changeText5(){
+	document.getElementById("headerChange").innerHTML = "Lorem Delta";
+	document.getElementById("paragraphChange").innerHTML = "Nullam sed libero malesuada, consectetur sapien a, laoreet ex. Vivamus volutpat ligula id elementum tristique. Nulla facilisi. Integer sodales sapien lacus, vitae auctor ex accumsan id. Nunc lobortis nibh laoreet augue lacinia, bibendum dignissim sem tempus. Donec gravida scelerisque nibh in convallis. Pellentesque aliquam porta mauris, in varius mauris facilisis vitae. Donec blandit tincidunt velit, quis tincidunt leo efficitur pharetra.";
+    
+    banner.style.background = '#F7CB2F';
+    paragraph.style.color = '#222222'
+}
+
+function changeText6(){
+	document.getElementById("headerChange").innerHTML = "Lorem Omega";
+	document.getElementById("paragraphChange").innerHTML = "Phasellus auctor mollis lacus, in lobortis leo tincidunt vel. Nam at pretium ipsum. Nullam nulla leo, auctor quis mattis vel, malesuada sit amet tellus. Etiam vel placerat eros, in suscipit justo. Nullam luctus lacus vel ex ultrices, ac maximus diam sodales. Pellentesque non sapien eget ipsum gravida fringilla ut vehicula eros. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam pharetra lobortis lacus, a ultrices urna ornare pulvinar.";
+    
+    banner.style.background = '#ED742D';
+    paragraph.style.color = '#222222'
+}
+
